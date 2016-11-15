37b7e2832efacb7eb93ae0e0f3e848d3752ad73a
ddindi <david.dindi@gmail.com>
2016-11-15 22:51:38 +0100

    udpate to provide name

--- fetch/download_json.py
+++ fetch/download_json.py
@@ -10,11 +10,11 @@ def download_json(input_link = 'https://api.github.com/search/repositories?q=lan
 	for i, item in enumerate(data['items']):
 		if i == max_repos: 
 			break
-		yield item['clone_url']
+		yield item['name'], item['clone_url']
 
 def main():
-	for link in download_json(): 
-		print link
+	for name, clone_url in download_json(): 
+		print name, clone_url
 
 if __name__ == '__main__':
     main()
